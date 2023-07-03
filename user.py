8b3a38fd4ce7c236593d2737b06a4374f8258668
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-07-03 20:01:44 -0300

    add tentativas de login

--- user.py
+++ user.py
@@ -1,13 +1,23 @@
 class User:
-    def __init__(self, first_name, last_name, nickname):
+    def __init__(self, first_name, last_name, nickname, login_attempts=0):
         self.first_name = first_name
         self.last_name = last_name
         self.nickname = nickname
+        self.login_attempts = login_attempts
     
     
     def describe_user(self):
         print(f'{self.nickname}: {self.first_name} {self.last_name}')
+        print(f'Tentativas de login: {self.login_attempts}')
+    
+    
+    def increment_login_attempts(self):
+        self.login_attempts += 1
         
+    
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+    
     
     def greet_user(self):
         print(f'Olá, {self.nickname}!')
@@ -17,3 +27,8 @@ user01 = User('Ana', 'Morais', 'anamorais')
 user02 = User('Beatriz', 'Sousa', 'biasousa')
 user01.describe_user()
 user02.greet_user()
+user01.increment_login_attempts()
+user01.increment_login_attempts()
+user01.describe_user()
+user01.reset_login_attempts()
+user01.describe_user()
